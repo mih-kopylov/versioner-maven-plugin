7a6ab988f9ef5d0f92302b30690ff3b25c313a06
Mikhail Kopylov <mih.kopylov@yandex.ru>
2018-09-03 23:47:57 +0300

    Use jgit to create a branch

--- src/main/java/ru/omickron/actions/BranchAction.java
+++ src/main/java/ru/omickron/actions/BranchAction.java
@@ -1,30 +1,16 @@
 package ru.omickron.actions;
 
-import javax.inject.Inject;
+import java.io.File;
 import javax.inject.Named;
 import lombok.NonNull;
 import lombok.SneakyThrows;
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.BuildPluginManager;
-
-import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.element;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.executeMojo;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.executionEnvironment;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.goal;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
+import org.eclipse.jgit.api.Git;
 
 @Named
 public class BranchAction {
-    @Inject
-    private MavenSession mavenSession;
-    @Inject
-    private BuildPluginManager buildPluginManager;
-
     @SneakyThrows
     public void create( @NonNull String branchName ) {
-        executeMojo( plugin( "org.apache.maven.plugins", "maven-scm-plugin", "1.10.0" ), goal( "branch" ),
-                configuration( element( "basedir", "." ), element( "branch", branchName ) ),
-                executionEnvironment( mavenSession, buildPluginManager ) );
+        Git git = Git.open( new File( "./.git" ) );
+        git.branchCreate().setName( branchName ).call();
     }
 }
