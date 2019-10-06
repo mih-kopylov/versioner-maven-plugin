afb017201710b8cc2b9076370272aa373e1fe0bd
Mikhail Kopylov <mih.kopylov@yandex.ru>
2019-10-06 15:50:13 +0300

    Change CommitActor to commit not only `pom.xml` files, but all changed ones.
    This is required if some Actors affect not only `pom.xml` file and the changes should be committed.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased](https://github.com/mih-kopylov/versioner-maven-plugin/compare/3.1.0...HEAD)
+### Changed
+- CommitActor commits now not only `pom.xml` files, but all changed files
 
 ## [3.1.0](https://github.com/mih-kopylov/versioner-maven-plugin/compare/3.0.0...3.1.0) - 2019.10.05
 ### Added
--- src/main/java/ru/mihkopylov/actor/CommitActor.java
+++ src/main/java/ru/mihkopylov/actor/CommitActor.java
@@ -1,18 +1,15 @@
 package ru.mihkopylov.actor;
 
-import java.io.File;
 import java.util.Objects;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.SneakyThrows;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.Status;
 import ru.mihkopylov.service.GitService;
 
 @Named
 public class CommitActor implements Actor {
-    private static final String SUFFIX = "pom.xml";
     @Inject
     private GitService gitService;
 
@@ -20,20 +17,9 @@ public class CommitActor implements Actor {
     @SneakyThrows
     @Nullable
     public String act( @Nullable String message ) {
-        Status status = gitService.git().status().call();
-        //add only poms to the commit as only they should be changed
-        status.getModified().stream().filter( o -> o.endsWith( SUFFIX ) ).forEach( this :: addFile );
-        gitService.git().commit().setMessage( Objects.requireNonNull( message ) ).call();
+        Git git = gitService.git();
+        git.add().addFilepattern( "." ).call();
+        git.commit().setMessage( Objects.requireNonNull( message ) ).call();
         return null;
-    }
-
-    @SneakyThrows
-    private void addFile( String o ) {
-        gitService.git().add().addFilepattern( o ).call();
-    }
-
-    @SneakyThrows
-    private Git createGit() {
-        return Git.open( new File( "./.git" ) );
     }
 }
